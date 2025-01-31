671e9c66050432f7ac66d5c2790083cf05e6a98f
Phaney Phin <phenpani@gmail.com>
2025-01-31 09:33:17 +0700

    Add webhook event for revoke

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withNextra = require('nextra')({
-    output: 'export',
+  output: 'standalone', // https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx'
   })
